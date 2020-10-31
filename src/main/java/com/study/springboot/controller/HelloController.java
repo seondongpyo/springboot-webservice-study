61413e058e95426761525c58fb36df6e026ac6f2
Seondong Pyo <psd1390@gmail.com>
2020-11-01 01:03:57 +0900

    Dto 추가 및 테스트 코드 작성

--- src/main/java/com/study/springboot/controller/HelloController.java
+++ src/main/java/com/study/springboot/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.study.springboot.controller;
 
+import com.study.springboot.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello Spring Boot!";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/study/springboot/controller/HelloControllerTest.java
+++ src/test/java/com/study/springboot/controller/HelloControllerTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = HelloController.class)
 class HelloControllerTest {
@@ -16,10 +17,22 @@ class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    @DisplayName("/hello로 접근 시 'Hello Spring Boot!'가 리턴된다")
-    void helloController() throws Exception {
+    @DisplayName("/hello 테스트")
+    void hello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/hello"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Hello Spring Boot!"));
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Spring Boot!"));
+    }
+
+    @Test
+    @DisplayName("/hello/dto 테스트")
+    void helloDto() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/hello/dto")
+                    .param("name", "hello") // param()은 String만 가능하다
+                    .param("amount", String.valueOf(1000)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("hello"))) // jsonPath() : JSON 응답 값을 필드별로 검증 가능
+                .andExpect(jsonPath("$.amount", is(1000))); // $를 기준으로 필드명을 명시한다
     }
 }
